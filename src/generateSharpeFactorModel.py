d6a2cc292a0573bd539743704c75929e7025db4a
Hobey Kuhn <hkuhn@umich.edu>
2015-05-08 11:00:01 -0400

    fixed it so that i store all data in a data frame ticker vs time

--- src/generateSharpeFactorModel.py
+++ src/generateSharpeFactorModel.py
@@ -6,11 +6,18 @@ dataLocations = read_csv('config/dataLocations.csv', encoding='utf-8')
 
 # read data and store in a data frame
 #	master_returns_data
-#		[[ticker (object)], [csv data (DataFrame)]]
-master_returns_data = []
+#		[ ticker vs date and adj close]
+monthly_data = read_csv(dataLocations['path'].iloc[0])
+master_returns_data = DataFrame(index=monthly_data['Date'], columns=dataLocations['ticker'])
 for index, row in dataLocations.iterrows():
 	monthly_data = read_csv(row['path'])
-	new_row = [row['ticker'], monthly_data]
-	master_returns_data.append(new_row)
+	returns = monthly_data.set_index('Date')['Adj Close'].to_dict()
+	returns_series = Series(returns)
+	master_returns_data[row['ticker']] = returns_series
 
+# get market data
+market_data = master_returns_data['SPY']
 
+# compute parameters (betas, variance) of assets
+for item in master_returns_data:
+	asset_return_matrix = item[1]['Adj Close']
